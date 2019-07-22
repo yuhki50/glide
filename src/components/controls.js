aad656bbe213771f973c72474c0f99afd4b1dace
Daniel Korte <daniel@danielkorte.com>
2019-07-22 15:37:09 -0500

    fix(controls): Chrome error 'Unable to preventDefault inside passive event listener' (#351)
    (cherry picked from commit 4f8f538b15a5fa393ee3b1498ef0a3d885dcce2f)

--- src/components/controls.js
+++ src/components/controls.js
@@ -155,7 +155,9 @@ export default function (Glide, Components, Events) {
      * @return {Void}
      */
     click (event) {
-      event.preventDefault()
+      if (!supportsPassive && event.type === 'touchstart') {
+        event.preventDefault()
+      }
 
       Components.Run.make(Components.Direction.resolve(event.currentTarget.getAttribute('data-glide-dir')))
     }
